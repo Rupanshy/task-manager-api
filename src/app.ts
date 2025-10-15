d14e35e4a51c483ac1a70871b27b2cc571ec48ea
Rupanshy Sharma <rupanshysharma@Rupanshys-MacBook-Pro.local>
2025-10-15 21:16:40 +0530

    Setting auth routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import morgan from "morgan";
+import { authRouter } from "./auth-service/routes/authRoute.js"; 
 
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/api/auth", authRouter);
 
 app.get("/health", (req, res) => {
     res.json({status: "ok"});
